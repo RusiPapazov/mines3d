fff443d945a2d1b34e251a9115f34bd36e008a9f
Rusi Papazov <rusi.papazov@jiminny.com>
2023-02-27 21:09:01 +0200

    delete jquery dependencies

--- js/app.js
+++ js/app.js
@@ -2,24 +2,30 @@
 /*jslint browser: true */
 (function () {
     "use strict";
-    var env,
-        webglAvailable = function webglAvailable() {
-            var canvas;
-            try {
-                canvas = document.createElement('canvas');
-                return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
-            } catch (e) {
-                return false;
-            }
-        };
-    env = Detector.webgl && webglAvailable() ? 3 : 2;
-    define(['./app' + env + 'D'], function () {
-        var head  = document.getElementsByTagName('head')[0],
-            link  = document.createElement('link');
-        link.rel  = 'stylesheet';
-        link.type = 'text/css';
-        link.href = './css/styles' + env + 'd.css';
-        link.media = 'all';
-        head.appendChild(link);
-    });
+    function webglAvailable() {
+        try {
+            const canvas = document.createElement('canvas');
+
+            return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+
+        } catch (e) {
+            return false;
+        }
+    }
+
+    if (Detector.webgl && webglAvailable()) {
+        define(['./app3D'], function () {
+            const head  = document.getElementsByTagName('head')[0];
+            const link  = document.createElement('link');
+
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = './css/styles3d.css';
+            link.media = 'all';
+            head.appendChild(link);
+        });
+    } else {
+        document.body.innerText = 'WebGl is required.';// @todo add more info
+    }
+    // env = Detector.webgl && webglAvailable() ? 3 : 2;
 }());
